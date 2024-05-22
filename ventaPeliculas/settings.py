97800ecbbfb2b54c614756df4328eb869ed10039
Zare <zarellaburbanov@gmail.com>
2024-05-22 17:50:38 -0500

    elimiacion link

--- ventaPeliculas/settings.py
+++ ventaPeliculas/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000', 'electivaiiback.azurewebsites.net'
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'ventaPeliculas.urls'

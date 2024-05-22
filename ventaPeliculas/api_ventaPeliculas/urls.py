d9b123966d27e0fd1270dc4486ab9d6b5c830555
Zare <zarellaburbanov@gmail.com>
2024-05-22 17:11:20 -0500

    ajustes

--- ventaPeliculas/api_ventaPeliculas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PeliculaView
-
-urlpatterns= [
-    path('creacionPelicula', PeliculaView.as_view(), name='creacionPelicula'),
-    path('actualizacionPelicula/<int:pkid>/',PeliculaView.as_view(), name='actualizacion_pelicula'), #put
-    path('eliminacionPelicula/<int:pkid>/',PeliculaView.as_view(), name='eliminacion_pelicula'), #delete
-]
--- ventaPeliculas/ventaPeliculas/urls.py
+++
@@ -1,9 +0,0 @@
-# project/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/peliculas/', include('api_ventaPeliculas.urls')),
-]
-
